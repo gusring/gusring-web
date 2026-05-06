eabcedeedbf4c6cd6437ff0b91c46743bc8b5e87
minsik1014 <ms03zzang@gmail.com>
2026-05-06 10:17:33 +0900

    fix: 빌드오류 수정

--- src/data/languages.ts
+++ src/data/languages.ts
@@ -1,16 +1,9 @@
 import { Language } from '../shared/types';
 
 export const languages: Language[] = [
-<<<<<<< HEAD
-  { id: 'ko', label: '한국어',     icon: '🇰🇷' },
-  { id: 'en', label: 'English',    icon: '🇺🇸' },
-  { id: 'zh', label: '中文',       icon: '🇨🇳' },
-  { id: 'ja', label: '日本語',     icon: '🇯🇵' },
-=======
-  { id: 'ko', label: '한국어',    icon: '🇰🇷' },
-  { id: 'en', label: 'English',  icon: '🇺🇸' },
-  { id: 'zh', label: '中文',     icon: '🇨🇳' },
-  { id: 'ja', label: '日本語',   icon: '🇯🇵' },
->>>>>>> 4003116c606cffc3cb8afb96a90b2521e910ab23
+  { id: 'ko', label: '한국어', icon: '🇰🇷' },
+  { id: 'en', label: 'English', icon: '🇺🇸' },
+  { id: 'zh', label: '中文', icon: '🇨🇳' },
+  { id: 'ja', label: '日本語', icon: '🇯🇵' },
   { id: 'vi', label: 'Tiếng Việt', icon: '🇻🇳' },
 ];
